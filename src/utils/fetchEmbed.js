30f4d294930a3314114c6d25750fb9aeb15199b3
Dong <ndaidong@gmail.com>
2019-08-27 17:59:49 +0700

    v1.3.1
    - Remove try/catch block from fetchEmbed function
    
    Related issues: #46

--- src/utils/fetchEmbed.js
+++ src/utils/fetchEmbed.js
@@ -3,29 +3,24 @@
 const fetch = require('cross-fetch').default;
 
 const fetchEmbed = async (url, provider, params) => {
-  try {
-    let {
-      provider_name, // eslint-disable-line camelcase
-      provider_url, // eslint-disable-line camelcase
-      url: resourceUrl,
-    } = provider;
+  let {
+    provider_name, // eslint-disable-line camelcase
+    provider_url, // eslint-disable-line camelcase
+    url: resourceUrl,
+  } = provider;
 
 
-    resourceUrl = resourceUrl.replace(/\{format\}/g, 'json');
+  resourceUrl = resourceUrl.replace(/\{format\}/g, 'json');
 
-    let link = `${resourceUrl}?format=json&url=${encodeURIComponent(url)}`;
-    link = params && params.maxwidth ? `${link}&maxwidth=${params.maxwidth}` : link;
-    link = params && params.maxheight ? `${link}&maxheight=${params.maxheight}` : link;
+  let link = `${resourceUrl}?format=json&url=${encodeURIComponent(url)}`;
+  link = params && params.maxwidth ? `${link}&maxwidth=${params.maxwidth}` : link;
+  link = params && params.maxheight ? `${link}&maxheight=${params.maxheight}` : link;
 
-    const res = await fetch(link, {mode: 'no-cors'});
-    const json = await res.json();
-    json.provider_name = provider_name; // eslint-disable-line camelcase
-    json.provider_url = provider_url; // eslint-disable-line camelcase
-    return json;
-  } catch (err) {
-    console.trace(err);
-    return null;
-  }
+  const res = await fetch(link, {mode: 'no-cors'});
+  const json = await res.json();
+  json.provider_name = provider_name; // eslint-disable-line camelcase
+  json.provider_url = provider_url; // eslint-disable-line camelcase
+  return json;
 };
 
 module.exports = fetchEmbed;
