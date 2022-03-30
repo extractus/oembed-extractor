bd7cdae3c7144a552b7e3d182d7bbd682db09604
Adilet <i.adilets@gmail.com>
2022-03-30 17:17:58 +0600

    Update oembed-parser.js

--- dist/cjs/oembed-parser.js
+++ dist/cjs/oembed-parser.js
@@ -6876,6 +6876,7 @@ var providers = [
       {
         "schemes": [
           "https://*.youtube.com/shorts*",
+          "https://youtube.com/shorts*",
           "https://*.youtube.com/watch*",
           "https://*.youtube.com/v/*",
           "https://youtu.be/*",
