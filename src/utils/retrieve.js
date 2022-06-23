d0bab6205b3147091ccb22b5eb49e930554de63f
Dong Nguyen <ndaidong@gmail.com>
2022-06-23 14:21:22 +0700

    v3.0.5
    - Fix Instagram content type filter
    - Update dependencies

--- src/utils/retrieve.js
+++ src/utils/retrieve.js
@@ -3,18 +3,28 @@
 import axios from 'axios'
 
 import { getRequestOptions } from '../config.js'
+import { error } from './logger.js'
+
+const isValidContentType = (ctype) => {
+  return [
+    'application/json',
+    'text/javascript'
+  ].some((item) => {
+    return ctype.includes(item)
+  })
+}
 
 export default async (url) => {
   try {
     const res = await axios.get(url, getRequestOptions())
-
     const contentType = res.headers['content-type'] || ''
-    if (!contentType || !contentType.includes('application/json')) {
+    if (!isValidContentType(contentType)) {
+      error(`Invalid content type! (${contentType})`)
       return null
     }
-
     return res.data
   } catch (err) {
+    error(err.message)
     return null
   }
 }
